cb98d7074e971df1dc309112183b130ce77da31d
manena <alireza7192@gmail.com>
2018-10-14 23:46:34 +0330

    basic webserver

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/go-playground/webhooks.v5/github"
+	"net/http"
+)
+
+const (
+	path = "/hooks"
+	hookSecret = "1qaz2wsx#EDC$RFV"
+)
+
+func main() {
+
+	http.HandleFunc(path, webhookHandler)
+	http.ListenAndServe(":4444", nil)
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	hook, _ := github.New(github.Options.Secret(hookSecret))
+	payload, err := hook.Parse(r, github.PullRequestEvent, github.PushEvent)
+	if err != nil {
+		if err == github.ErrEventNotFound {
+			fmt.Println("does this work?")
+		}
+	}
+	switch payload.(type) {
+
+	case github.PullRequestPayload:
+		pullRequest := payload.(github.PullRequestPayload)
+		// Do whatever you want from here...
+		fmt.Printf("%+v", pullRequest)
+
+	case github.PushPayload:
+		push := payload.(github.PushPayload)
+		fmt.Printf("%+v", push)
+	}
+}
