410f8dd0c3d688d0af87a9194977b5c2a7a2584e
manena <alireza7192@gmail.com>
2018-11-01 19:50:50 +0330

    meh

--- main.go
+++ main.go
@@ -117,7 +117,7 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//moooooooooooooooooooo
+//mooooooooooooooooooooooo
 func createCheckRun(head string) CreateCheckRunPayload {
 	checkRun := CreateCheckRunPayload{
 		Name:        "First check run test",
